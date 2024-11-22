33a51726cc473bc3985a652cdd469abcc4d16921
boubaker-abderrahman <boubakerabderrahman2003@gmail.com>
2024-11-22 19:36:17 +0100

    Deployment ready

--- backend/server.js
+++ backend/server.js
@@ -7,21 +7,29 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import { app, server } from './socket/socket.js'
 import express from "express";
+import path  from 'path';
 
+const PORT =process.env.PORT || 5000
+const __dirname = path.resolve()
 
 dotenv.config()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({ origin : "http://localhost:5173" , credentials : true}));
+
 
 
 app.use('/api/auth' , authRouter)
 app.use('/api/messages' , messageRoute)
 app.use('/api/users' , userRoute)
 
+app.use(express.static(path.join(__dirname,"frontend/dist")))
 
-server.listen(5000, ()=>{
+app.get("*" , (req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
+
+server.listen(PORT, ()=>{
     connectDb()
-    console.log('listening on port 5000')
+    console.log('listening on port ',PORT)
 } )
